edc73aadfcb1d85c59ba1cecf2e8c9399873411b
Maneesh Tewani <mtewani@google.com>
2025-03-10 15:59:58 -0700

    Updated template

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,56 +1,45 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
-  ];
-
-  idx.internal.templates-cli.enable = true;
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
+{ pkgs, ... }: 
+  {
+    channel = "stable-24.05";
+    packages = [
+      pkgs.nodejs_20
     ];
-
-    # Enable previews
-    previews = {
+    
+    env = {
+      FIREBASE_DATACONNECT_POSTGRESQL_STRING =  "postgresql://user:mypassword@localhost:5432/dataconnect?sslmode=disable";
+    };
+  
+    services.postgres = {
+      extensions = ["pgvector"];
       enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
     };
 
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+    idx = {
+      extensions = [
+        "mtxr.sqltools"
+        "mtxr.sqltools-driver-pg"
+        "GraphQL.vscode-graphql-syntax"
+        "GoogleCloudTools.firebase-dataconnect-vscode"
+      ];
+      workspace = {
+        onCreate = {
+          update-firebase = "npm install -g firebase-tools";
+          postgres = ''
+            psql --dbname=postgres -c "ALTER USER \"user\" PASSWORD 'mypassword';"
+            psql --dbname=postgres -c "CREATE DATABASE dataconnect;"
+            psql --dbname=dataconnect -c "CREATE EXTENSION vector;"
+          '';
+          npm-install = "cd app && npm i";
+        };
       };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+      previews = {
+        enable = true;
+        previews = {
+          web = {
+            command = ["npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0"];
+            manager = "web";
+          };
+        };
       };
     };
-  };
 }
