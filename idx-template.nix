71679ed9a2e8051b91fe46063105d23ddef47edc
Maneesh Tewani <mtewani@google.com>
2025-03-10 19:51:44 -0700

    Update for IDX

--- idx-template.nix
+++ idx-template.nix
@@ -13,12 +13,11 @@
   bootstrap = ''
     mkdir "$out" 
     mkdir "$out"/.idx
-    mkdir "$out"/app
     mkdir "$out"/dataconnect
     mkdir "$out"/.vscode
 
     cp ${.idx/dev.nix} "$out"/.idx/dev.nix
-    cp -a ${./app}/* "$out"/app/
+    cp -a ./* "$out"/
     cp -a ${./dataconnect}/* "$out"/dataconnect/
 
     cp ${./.vscode/settings.json} "$out"/.vscode/settings.json
